d21122f45a0a3039744aa8e86ea289a661858be7
Imran Khan <99342205+imranrkhan13@users.noreply.github.com>
2025-11-27 18:21:09 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import './index.css';
 import { CartProvider } from "./context/CartContext";
-import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <CartProvider>
