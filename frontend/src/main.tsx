374d987127a68515b8a8136080e52ed81c6df0dd
Imran Khan <99342205+imranrkhan13@users.noreply.github.com>
2025-12-03 14:10:15 +0530

    Add Toaster component from react-hot-toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import './index.css';
 import { CartProvider } from "./context/CartContext";
+import { Toaster} from 'react-hot-toast';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <CartProvider>
