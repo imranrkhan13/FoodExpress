33d52c092e64a2055f440c342e4e3f172eaff457
Imran Khan <99342205+imranrkhan13@users.noreply.github.com>
2025-12-04 22:22:29 +0530

    Update restaurantApi.ts

--- frontend/src/api/restaurantApi.ts
+++ frontend/src/api/restaurantApi.ts
@@ -1,25 +1,13 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
-export const getRestaurants = async (
-  page: number,
-  limit: number,
-  filters: any
-) => {
-  const params: any = {
-    page,
-    limit,
-    ...filters,
-  };
 
-  const res = await axios.get(`${API_BASE_URL}/restaurants`, { params });
-  return res.data;
-};
-;
-
-
-
-export const getRestaurantById = async (id: string) => {
-  const res = await axios.get(`${API_BASE_URL}/restaurants/${id}`);
-  return res.data.data; // single restaurant
+export const getRestaurants = async (page: number) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/restaurants?page=${page}`);
+    return response.data;  // Should be the JSON from backend
+  } catch (error) {
+    console.error("Error fetching:", error);
+    throw error;
+  }
 };
