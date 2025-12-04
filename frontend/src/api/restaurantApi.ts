39081fc4e2bf9fa260c273c5f771fdb5f6f2112d
Imran Khan <99342205+imranrkhan13@users.noreply.github.com>
2025-12-04 22:27:55 +0530

    Refactor error message in getRestaurants function
    Updated error logging message for fetching restaurants.

--- frontend/src/api/restaurantApi.ts
+++ frontend/src/api/restaurantApi.ts
@@ -2,18 +2,16 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-// Get all restaurants
 export const getRestaurants = async (page: number) => {
   try {
     const response = await axios.get(`${API_BASE_URL}/restaurants?page=${page}`);
     return response.data;
   } catch (error) {
-    console.error("Error fetching restaurants:", error);
+    console.error("Error fetching:", error);
     throw error;
   }
 };
 
-// ✅ Add this function for a single restaurant
 export const getRestaurantById = async (id: string) => {
   try {
     const response = await axios.get(`${API_BASE_URL}/restaurants/${id}`);
